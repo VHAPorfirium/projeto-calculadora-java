facbf40e89093e1a13fc2d79111f176dc52f8813
Victor Hugo <vhaporfiro@gmail.com>
2025-08-28 20:57:09 -0300

    Criação da função de somar dois numeros em java.

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.somar(5, 3);
+        System.out.println("O resultado da soma é: " + resultado);
+
+    }
+}
