230bb27ff7a414812c74ef9674847957854c4c99
Kxique14 <granatokxique@gmail.com>
2025-08-28 21:16:33 -0300

    Adição da função de subtração e multiplicação

--- Main.java
+++ Main.java
@@ -5,5 +5,10 @@ public class Main {
         int resultado = calculadora.somar(5, 3);
         System.out.println("O resultado da soma é: " + resultado);
 
+        int resultado2 = calculadora.subtrair(6,3);
+        System.out.println("Resultado da subtração é: " + resultado2);
+
+        int resultado3 = calculadora.multiplicacao(8,3);
+        System.out.println("Resultado da multiplicação é: " + resultado3);
     }
 }
